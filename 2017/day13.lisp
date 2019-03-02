6b9e5ffdcbfda6485f451a0b63bb83649fe1bde1
Matteo Landi <matteo@matteolandi.net>
2019-03-02 17:05:02 +0100

    Optimize 2017 day13 solution
    Don't simulate, do the math to understand where and when the check would
    happen (from 13secs to 0.4 secs)

--- 2017/day13.lisp
+++ 2017/day13.lisp
@@ -18,43 +18,32 @@
                          :dir +1))))
     (mapcar #'parse-layer x)))
 
-(defun update-layers (layers)
-  (dolist (l layers)
-    (let* ((next-cur (+ (layer-curr l) (layer-dir l))))
-      (cond ((>= next-cur (layer-range l))
-             (setf (layer-dir l) -1))
-            ((< next-cur 0)
-             (setf (layer-dir l) 1)))
-      (setf (layer-curr l) (+ (layer-curr l) (layer-dir l))))))
-
-(defun copy-layers (layers)
-  (mapcar #'copy-structure layers))
-
-(defun trip-severity (layers depth-max)
+(defun trip-severity (layers)
   (loop
-    :for depth :to depth-max
-    :for layer = (find depth layers :key #'layer-depth)
-    :when (and layer (zerop (layer-curr layer)))
-    :sum (* (layer-depth layer) (layer-range layer))
-    :do (update-layers layers)))
+    :for layer :in layers
+    :for depth = (layer-depth layer)
+    :for time-to-layer = depth
+    :for range = (layer-range layer)
+    :for period = (* (1- range) 2)
+    :when (zerop (mod time-to-layer period))
+    :sum (* depth range))) 
 
-(defun caughtp (layers depth-max)
+(defun caughtp (layers delay)
   (loop
-    :for depth :to depth-max
-    :for layer = (find depth layers :key #'layer-depth)
-    :thereis (and layer (zerop (layer-curr layer)))
-    :do (update-layers layers)))
+    :for layer :in layers
+    :for depth = (layer-depth layer)
+    :for time-to-layer = (+ delay depth)
+    :for range = (layer-range layer)
+    :for period = (* (1- range) 2)
+    :thereis (zerop (mod time-to-layer period))))
 
 (define-problem (2017 13) (data parse-layers)
-  (let ((max-depth (reduce #'max data :key #'layer-depth)))
-    (values
-      (trip-severity (copy-layers data) max-depth)
-      (loop
-        :with layers = (copy-layers data)
-        :for delay = 1 :then (1+ delay)
-        :do (update-layers layers)
-        :unless (caughtp (copy-layers layers) max-depth)
-        :do (return delay)))))
+  (values
+    (pr (trip-severity data))
+    (loop
+      :for delay = 1 :then (1+ delay)
+      :unless (caughtp data delay)
+      :do (return delay))))
 
 (1am:test test-2017/13
   (multiple-value-bind (part1 part2) (problem-run)
