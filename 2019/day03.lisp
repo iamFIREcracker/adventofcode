2583da4a91dae954af87462f4ef6f9ddbf909f80
Matteo Landi <matteo@matteolandi.net>
2019-12-06 18:32:37 +0100

    Simplify 2019 day 03

--- 2019/day03.lisp
+++ 2019/day03.lisp
@@ -1,67 +1,42 @@
 (defpackage :aoc/2019/03 #.cl-user::*aoc-use*)
 (in-package :aoc/2019/03)
 
-(defun steps (d n)
-  "XXX
-  CL-USER> (make-list 10 :initial-element 8)
-   (8 8 8 8 8 8 8 8 8 8)"
-  (cond ((string= "R" d) (loop :for i :to (1- n) :collecting (complex 1)))
-        ((string= "U" d) (loop :for i :to (1- n) :collecting (complex 0 1)))
-        ((string= "L" d) (loop :for i :to (1- n) :collecting (complex (- 1) 0)))
-        ((string= "D" d) (loop :for i :to (1- n) :collecting (complex 0 (- 1))))))
-
 (defun parse-direction (str)
   (let ((d (subseq str 0 1))
         (n (parse-integer (subseq str 1))))
-    (steps d n)))
-
-(defun flatten (ls)
-  "Borrowed from Paul Graham's OnLisp"
-  (labels ((mklist (x) (if (listp x) x (list x))))
-    (mapcan #'(lambda (x) (if (atom x) (mklist x) (flatten x))) ls)))
+    (cond ((string= "R" d) (make-list n :initial-element (complex 1)))
+          ((string= "U" d) (make-list n :initial-element (complex 0 1)))
+          ((string= "L" d) (make-list n :initial-element (complex (- 1) 0)))
+          ((string= "D" d) (make-list n :initial-element (complex 0 (- 1)))))))
 
 (defun read-wires (data)
   (loop
-    :for str :in data
-    :for list = (split-sequence:split-sequence #\, str)
-    :for list-of-steps = (mapcar #'parse-direction list)
-    :collecting (flatten list-of-steps)))
+    :for wire :in data
+    :for list = (split-sequence:split-sequence #\, wire)
+    :for list-of-steps = (reduce #'append (mapcar #'parse-direction list))
+    :collecting list-of-steps))
 
-(defun solve-part1 (wires &aux (seen (make-hash-table)))
+(defun walk (wires &optional part2 &aux (seen (make-hash-table)))
   (loop
-    :with curr = #C(0 0)
     :for d :in (first wires)
-    :do (incf curr d)
-    :do (setf (gethash curr seen) curr))
-  (loop
-    :with curr = #C(0 0)
-    :for d :in (second wires)
-    :do (incf curr d)
-    :when (gethash curr seen)
-    :collect it :into cross
-    :finally (return (minimization cross :key (partial-1 #'manhattan-distance _ #C(0 0))))))
-
-(defun solve-part2 (wires &aux (seen (make-hash-table)))
-  (loop
-    :with curr = #C(0 0)
-    :for d :in (first wires)
+    :for curr = d :then (+ curr d)
     :for n = 1 :then (+ n 1)
-    :do (incf curr d)
     :when (not (gethash curr seen))
     :do (setf (gethash curr seen) n))
   (loop
-    :with curr = #C(0 0)
     :for d :in (second wires)
+    :for curr = d :then (+ curr d)
     :for n = 1 :then (+ n 1)
-    :do (incf curr d)
-    :when (gethash curr seen)
-    :collect (+ n (gethash curr seen)) :into cross
-    :finally (return (minimization cross))))
+    :for distance = (gethash curr seen)
+    :when distance :collect (if part2 (+ distance n) curr) :into crosses
+    :finally (return (minimization crosses :key (if part2
+                                                  #'identity
+                                                  (partial-1 #'manhattan-distance _ #C(0 0)))))))
 
 (define-problem (2019 3) (wires read-wires)
   (values
-    (solve-part1 wires)
-    (solve-part2 wires)))
+    (walk wires)
+    (walk wires T)))
 
 (1am:test test-2019/03
   (multiple-value-bind (part1 part2) (problem-run)
