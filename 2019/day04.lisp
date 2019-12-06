1d5a1fd0974f5244c460ddd472c427817ce4aa5b
Matteo Landi <matteo@matteolandi.net>
2019-12-06 18:30:20 +0100

    Fix bug with 2019 day 04 -- stupid refactoring

--- 2019/day04.lisp
+++ 2019/day04.lisp
@@ -15,16 +15,14 @@
 
 (defun not-decreasing (digits)
   (loop
-    :for (d . remaining) :on digits
-    :for next = (first remaining)
+    :for (d . (next . remaining)) :on digits
     :while next
     :always (<= d next)))
 
 (defun at-least-one-digit-repeated (digits)
   "`DIGITS` are expected to be sorted"
   (loop
-    :for (d . remaining) :on digits
-    :for next = (first remaining)
+    :for (d . (next . remaining)) :on digits
     :while next
     :when (= d next) :return T))
 
@@ -32,13 +30,14 @@
   (loop
     :for password :from from :upto (1+ to)
     :for digits = (digits password)
-    :counting (and (not-decreasing digits) (at-least-one-digit-repeated digits))))
+    :counting (and
+                (not-decreasing digits)
+                (at-least-one-digit-repeated digits))))
 
 (defun at-least-digit-with-frequency-2 (digits)
-  "`DIGITS` are expected to be sorted"
   (loop
-    :for (d . remaining) :on digits
-    :thereis (= 1 (count d remaining))))
+    :for d :in digits
+    :thereis (= 2 (count d digits))))
 
 (defun solve-part2 (from to)
   (loop
@@ -47,7 +46,7 @@
     :counting (and
                 (not-decreasing digits)
                 (at-least-one-digit-repeated digits)
-                (at-least-digit-with-frequency-2 digits))))
+                (at-least-one-digit-with-frequency-2 digits))))
 
 (define-problem (2019 4) (range read-range)
   (destructuring-bind (from to) range
