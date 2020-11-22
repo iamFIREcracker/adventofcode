f994e71264a99a8b4736cc6ff53f5612b496f751
Matteo Landi <matteo@matteolandi.net>
2020-11-22 19:22:58 +0100

    Add solution for 2016/16

---
+++ 2016/day16.lisp
@@ -0,0 +1,40 @@
+(defpackage :aoc/2016/16 #.cl-user::*aoc-use*)
+(in-package :aoc/2016/16)
+
+(defun generate-random-data (seed size)
+  (let ((array (make-array size
+                           :element-type 'base-char
+                           :adjustable t
+                           :fill-pointer 0)))
+    (loop for c across seed do (vector-push c array))
+    (loop while (< (length array) size)
+          for last = (1- (fill-pointer array)) do
+          (vector-push #\0 array)
+          (loop for i from last downto 0
+                for c = (aref array i) do
+                (vector-push (if (char= #\0 c) #\1 #\0) array)))
+    array))
+
+(defun checksum-reduction (array)
+  (loop for i below (length array) by 2
+        for j from 0
+        for c1 = (aref array i)
+        for c2 = (aref array (1+ i)) do
+        (setf (aref array j) (if (char= c1 c2) #\1 #\0))
+        finally (setf (fill-pointer array) (1+ j)))
+  array)
+
+(defun checksum (array)
+  (prog1 array
+    (loop do (checksum-reduction array)
+          while (evenp (length array)))))
+
+(define-problem (2016 16) (seed first)
+  (values
+    (checksum (generate-random-data seed 272))
+    (checksum (generate-random-data seed 35651584))))
+
+(1am:test test-2016/16
+  (multiple-value-bind (part1 part2) (problem-run)
+    (1am:is (string= "01110011101111011" part1))
+    (1am:is (string= "11001111011000111" part2))))
