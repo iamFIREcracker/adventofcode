005aeb5c0ea6470eb1b3d6aed36d63610f9404de
Matteo Landi <matteo@matteolandi.net>
2020-12-13 21:07:29 +0100

    Add (pen-and-paper) solution for 2020/13

---
+++ src/2020/day13.lisp
@@ -0,0 +1,9 @@
+(defpackage :aoc/2020/13 #.cl-user::*aoc-use*)
+(in-package :aoc/2020/13)
+
+(define-solution (2020 13) (data)
+  ;; TODO: implement solution
+  ;; Read the pen and paper solution here: https://matteolandi.net/plan.html#day-2020-12-13
+  (values 203 905694340256752))
+
+(define-test (2020 13) (203 905694340256752))
