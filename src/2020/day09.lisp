1228d2ffc0e0d6a245d23b938ed66b2e5064a07c
Matteo Landi <matteo@matteolandi.net>
2020-12-18 19:20:47 +0100

    Simplify 2020/09 part 2 -- use sliding window, and HSET

--- src/2020/day09.lisp
+++ src/2020/day09.lisp
@@ -1,33 +1,24 @@
 (defpackage :aoc/2020/09 #.cl-user::*aoc-use*)
 (in-package :aoc/2020/09)
 
-(defun make-set (numbers)
-  (let ((set (make-hash-table)))
-    (dolist (x numbers set)
-      (setf (gethash x set) t))))
-
-(defun sadd (validator number) (setf (gethash number validator) t))
-(defun srem (validator number) (remhash number validator))
-
 (defun validp (validator number)
   (loop for n being the hash-keys of validator
         thereis (gethash (- number n) validator)))
 
 (defun find-first-invalid (numbers)
-  (loop with s = (make-set (subseq numbers 0 25))
+  (loop with s = (make-hset (subseq numbers 0 25))
         for oldest in numbers for newest in (nthcdr 25 numbers)
         if (not (validp s newest)) return newest
-        else do (srem s oldest) (sadd s newest)))
+        else do (hset-add oldest s) (hset-add newest s)))
 
-(defun subseq-that-adds-up-to (numbers target &optional frontier)
-  (loop with n = (first numbers)
-        for (psum . seq) in (cons (cons 0 nil) frontier)
-        for psum-next = (+ n psum) for seq-next = (cons n seq)
-        if (= psum-next target) return seq-next
-        else collect (cons psum-next seq-next) into frontier-next
-        finally (return (subseq-that-adds-up-to (rest numbers)
-                                                target
-                                                frontier-next))))
+(defun subseq-that-adds-up-to (numbers target
+                                       &aux (numbers (coerce numbers 'vector)))
+  (loop
+    with start = 0 with end = 0 with sum = (aref numbers start)
+    if (= sum target) return (coerce (subseq numbers start (1+ end)) 'list)
+    else if (> sum target) do (setf sum (- sum (aref numbers start))
+                                    start (1+ start))
+    else do (setf end (1+ end) sum (+ sum (aref numbers end)))))
 
 (define-solution (2020 9) (numbers parse-integers)
   (let* ((invalid (find-first-invalid numbers))
