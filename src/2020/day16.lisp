3b6aaab5ac7e554c6705ef383155a5c48928cbc2
Matteo Landi <matteo@matteolandi.net>
2020-12-21 17:02:37 +0100

    Clean up 2020/16 solution (both part1 and part2)

--- src/2020/day16.lisp
+++ src/2020/day16.lisp
@@ -31,53 +31,64 @@
 (defun your-ticket (doc) (second doc))
 (defun nearby-tickets (doc) (third doc))
 
-(defun ticket-valid-p (ticket valid-numbers)
-  (every (lambda (n) (aref valid-numbers n)) ticket))
+(defun value-valid-p (rules value)
+  (loop for (name (from1 . to1) (from2 . to2)) in rules
+        thereis (or (<= from1 value to1)
+                    (<= from2 value to2))))
 
-(defun ticket-errors (ticket valid-numbers)
-  (loop for n across ticket unless (aref valid-numbers n) sum n))
+(defun ticket-errors (rules ticket)
+  (loop for value across ticket unless (value-valid-p rules value) sum value))
 
-(defun validate-tickets (doc)
-  (let ((valid-numbers (make-array 1000 :initial-element nil)))
-    (loop for (name . ranges) in (rules doc) do
-          (loop for (min . max) in ranges do
-                (loop for i from min upto max
-                      do (setf (aref valid-numbers i) name))))
-    (loop for ticket in (nearby-tickets doc)
-          sum (ticket-errors ticket valid-numbers) into total-error-rate
-          when (ticket-valid-p ticket valid-numbers) collect ticket into valid-tickets
-          finally (return (values valid-tickets
-                            total-error-rate)))))
+(defun ticket-scanning-error-rate (doc)
+  (reduce #'+ (nearby-tickets doc)
+          :key (lambda (ticket) (ticket-errors (rules doc) ticket))))
 
-(defun calculate-compatible-fields (rules tickets)
-  (let ((result (make-array (length rules) :initial-element nil)))
-    (dotimes (pos (length rules) result)
-      (loop for (name (from1 . to1) (from2 . to2)) in rules
-            for valid = (loop for ticket in tickets for n = (aref ticket pos)
-                              always (or (<= from1 n to1) (<= from2 n to2)))
-            when valid do (push name (aref result pos))))))
+(defun ticket-valid-p (rules ticket)
+  (loop for value across ticket always (value-valid-p rules value)))
 
-(defun find-field-mapping (compatibles &aux (n (length compatibles)))
-  (labels ((recur (mapping &aux (pos (length mapping)))
-             (cond
-               ((= pos n) (return-from find-field-mapping (reverse mapping)))
-               (t (loop for name in (set-difference
-                                      (aref compatibles pos)
-                                      mapping
-                                      :test 'string=) do
-                        (recur (cons name mapping)))))))
-    (recur nil)))
+(defun rules-to-ticket-positions (rules tickets &aux result)
+  (loop for pos below (length rules) do
+        (loop for (name (from1 . to1) (from2 . to2)) in rules
+              for valid = (loop for ticket in tickets
+                                for value = (aref ticket pos)
+                                always (or (<= from1 value to1)
+                                           (<= from2 value to2)))
+              for existing = (assoc name result :test #'string=)
+              when valid do
+              (if existing
+                (push pos (second existing))
+                (push (list name (list pos)) result)))
+        finally (return result)))
 
-(defun part2 (doc tickets)
-  (let* ((compatibles (calculate-compatible-fields (rules doc) tickets))
-         (mapping (find-field-mapping compatibles)))
-    (loop for name in mapping for part across (your-ticket doc)
-          when (search "departure" name) collect part into numbers
-          finally (return (reduce #'* numbers)))))
+(defun find-mapping (rules tickets)
+  (labels ((sort-rules (rules)
+             (sort rules #'< :key (lambda (x) (length (second x)))))
+           (rules-valid-p (rules)
+             (loop for (name positions) in rules
+                   always (> (length positions) 0)))
+           (update-rules (rules n p)
+             (loop for (name positions) in rules
+                   when (string/= name n)
+                   collect (list name (remove p positions))))
+           (recur (rules mapping &aux (rules (sort-rules rules)))
+             (cond ((not (rules-valid-p rules)) nil)
+                   ((null rules) (return-from find-mapping mapping))
+                   (t
+                     (loop for (n pp) in rules do
+                           (loop for p in pp do
+                                 (recur (update-rules rules n p)
+                                        (cons (cons n p) mapping))))))))
+    (recur (rules-to-ticket-positions rules tickets) nil)))
 
 (define-solution (2020 16) (doc parse-document)
-  (multiple-value-bind (valid-tickets error-rate)
-      (validate-tickets doc)
-    (values error-rate (swallow (part2 doc valid-tickets)))))
+  (values
+    (ticket-scanning-error-rate doc)
+    (let ((tickets (remove-if-not
+                     (lambda (ticket) (ticket-valid-p (rules doc) ticket))
+                     (nearby-tickets doc))))
+      (loop for (n . p) in (find-mapping (rules doc) tickets)
+            when (search "departure" n)
+            collect (aref (your-ticket doc) p) into values
+            finally (return (reduce #'* values))))))
 
 (define-test (2020 16) (27802 279139880759))
