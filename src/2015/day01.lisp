9dfedd77600bf3962c12a24aeb64cb887ec7574e
Matteo Landi <matteo@matteolandi.net>
2021-01-01 19:07:44 +0100

    Clean up 2015/01

--- src/2015/day01.lisp
+++ src/2015/day01.lisp
@@ -1,18 +1,21 @@
 (defpackage :aoc/2015/01 #.cl-user::*aoc-use*)
 (in-package :aoc/2015/01)
 
-(defun read-input (data) (first data))
+(defun parse-instruction (ch)
+  (ecase ch (#\( 1) (#\) -1)))
+
+(defun parse-input (lines)
+  (map 'list #'parse-instruction (first lines)))
 
 (defun part1 (instructions)
-  (loop for ch across instructions
-        sum (if (char= ch #\() 1 -1)))
+  (reduce #'+ instructions))
 
-(defun part2 (instructions)
-  (loop for ch across instructions and pos from 1
-        sum (if (char= ch #\() 1 -1) into floor
-        thereis (and (= floor -1) pos)))
+(defun part2 (instructions &aux (floor 0))
+  (loop for d in instructions and pos from 1
+        do (incf floor d)
+        when (= floor -1) return pos))
 
-(define-solution (2015 1) (instructions read-input)
+(define-solution (2015 1) (instructions parse-input)
   (values (part1 instructions) (part2 instructions)))
 
 (define-test (2015 1) (138 1771))
