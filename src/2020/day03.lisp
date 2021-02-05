74983aa094527b15087e44a00e836aa477ef7acb
Matteo Landi <matteo@matteolandi.net>
2021-02-05 18:40:27 +0100

    Fix compilation warning

--- src/2020/day03.lisp
+++ src/2020/day03.lisp
@@ -6,10 +6,27 @@
     (make-array (list rows columns) :initial-contents data)))
 
 (defun count-hits (map delta-col delta-row)
-  (loop with (rows cols) = (array-dimensions map)
-        for row below rows by delta-row
-        for col = 0 then (mod (+ col delta-col) cols)
-        count (char= (aref map row col) #\#)))
+  ;; I was previously destructuring `rows` and `cols` inside the for loop:
+  ;;
+  ;;   (loop with (rows cols) = (array-dimensions map)
+  ;;         ...
+  ;;
+  ;; That however gets expanded in such a way that the compiler would
+  ;; throw a style warning:
+  ;;
+  ;; - LOOP's destructuring bind would not signal an error if the list you are
+  ;; DESTRUCTURING-BIND against does not have enough elements; it would bind
+  ;; remaining variables to NIL instead
+  ;; - This means that both `rows` and `cols` can be NIL
+  ;; - `rows` is used after the keyword `:below`, and because of that it is
+  ;; expected to be a NUMBER
+  ;; - But NIL is not a NUMBER, hence the warning
+  ;;
+  ;; Technically the compiler is right, but what a pain...
+  (destructuring-bind (rows cols) (array-dimensions map)
+    (loop for row below rows by delta-row
+          for col = 0 then (mod (+ col delta-col) cols)
+          count (char= (aref map row col) #\#))))
 
 (define-solution (2020 3) (map parse-map)
   (values
