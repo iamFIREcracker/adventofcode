a3d95316fd9b4e379e14f01623d079121406f15d
Matteo Landi <matteo@matteolandi.net>
2021-12-07 15:41:27 +0100

    Add solution for 2021/07

---
+++ src/2021/day07.lisp
@@ -0,0 +1,19 @@
+(defpackage :aoc/2021/07 #.cl-user::*aoc-use*)
+(in-package :aoc/2021/07)
+
+
+(defun parse-crabs (data)
+  (mapcar #'parse-integer (cl-ppcre:all-matches-as-strings "\\d+" (first data))))
+
+
+(defun minimize-fuel (crabs distance-fun)
+  (loop for p from (find-min crabs) to (find-max crabs) minimize
+        (loop for c in crabs sum (funcall distance-fun c p))))
+
+
+(define-solution (2021 07) (crabs parse-crabs)
+  (values (minimize-fuel crabs (lambda (p1 p2) (abs (- p1 p2))))
+          (minimize-fuel crabs (lambda (p1 p2 &aux (n (abs (- p1 p2))))
+                                 (floor (* n (1+ n)) 2)))))
+
+(define-test (2021 07) (359648 100727924))
