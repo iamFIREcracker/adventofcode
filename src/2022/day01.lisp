1e48e17e423bbe5a607dd55644327b69b2b73f99
Matteo Landi <matteo@matteolandi.net>
2022-12-01 08:06:52 +0100

    Add solution for 2022/01

---
+++ src/2022/day01.lisp
@@ -0,0 +1,22 @@
+(defpackage :aoc/2022/01 #.cl-user::*aoc-use*)
+(in-package :aoc/2022/01)
+
+
+(defun elf-bags (strings &aux bs b)
+  (dolist (s (append strings '("")) bs)
+    (if (string= s "")
+      (setf bs (cons b bs) b nil)
+      (push (parse-integer s) b))))
+
+(defun bag-cals (bag) (reduce #'+ bag))
+
+(defun part1 (bags) (reduce #'max bags :key #'bag-cals))
+
+(defun part2 (bags)
+  (let ((cals (sort (mapcar #'bag-cals bags) #'>)))
+    (apply #'+ (subseq cals 0 3))))
+
+(define-solution (2022 01) (bags elf-bags)
+  (values (part1 bags) (part2 bags)))
+
+(define-test (2022 01) (75501 215594))
