91ac4d9af9c0042061eeeeecbf7b7c463f9d4ea7
Matteo Landi <matteo@matteolandi.net>
2022-12-04 14:35:26 +0100

    Changelogs

--- src/2022/day02.lisp
+++ src/2022/day02.lisp
@@ -1,17 +1,3 @@
-;;;; Version 0 (the one used for the stars):
-;;;; - Kept track not only of our score, but also the opponent's one (it was not required...)
-;;;; - For part 2, I ended up changing both the parsing and the looping logic (it's not required...see later)
-;;;; - [link](https://topaz.github.io/paste/#XQAAAQBWBwAAAAAAAAAd6AQKBhOR2ycIlwrqRiHywFJuxueW2QjzG+6/cZGkvU+2/1gn5WaRbwV6tIqysLuUZax8ticZmlBd5xQecpRPBgOiFTBwXnNx0FVsYvqVRQ5yHmUjD/PhdNzwUAvmypXUDyFwhRYfDmFjQ0ZCGKfHwzj0S6RN/ti0rvP8xhrQ2Yk4VcGTqEB3ptRC3xLXeNe70FXOfHrHUF4L0AdD3pzBKTc/f5Lpc2Eon5STrjYwZeSVvFjZWx991TlXDi4D/P+Cn67h2su3Ii0NwQZZhOXV4kvcZLt3Ao2Q3266mQhMEHFA4DN9259VMmIeVvmkBZ9jOEmje5M5/p2kMrbt6APSDAgVm/D78qBJ0p38XTTwqHTxVLuFJqJWMskOgdLXnPE66w7FtMBTOIGBpFF2n7ctMoQTkkccoMsZsLnOZkNDtCBgI9r0TDgtRzhc6l4uadx6hJsy5M7CVwy+G0c4pr3pIxjQ0/MbPqLudzBrdK8P+8qS41PEi83gucp3u0gw6EY6ZI86CKnYLFhEIegwYJ+h11tHh+YMXX1yOMi0qKgUGTLGvW06NvSPxkvnYeSgyEN+lpsesv1suLXjvkilDXyj/UW+dBKN7oNSWv4acuk=)
-;;;; Version 1:
-;;;; - Kept the looping logic the same, and only tweaked the parsing logic
-;;;; - Only _your_ socre is required, and if we don't have to store two scores anymore, then the LOOP form can be simplified a lot
-;;;; - [link](https://topaz.github.io/paste/#XQAAAQAkBAAAAAAAAAAd6AQKBhOR2ycIlwrqRiHywFJuxueW2QjzG+6/cZGkvU+2/1gn5WaRbwV6tIqysLuUZax8ticZmlBd5xQecpRPBgOiFTBwXnNx0FVsYvqVRQ5yHmUjD/PhdNzwUAvmypXUDyFwhRYfDmFjQ0ZCGKfHwzj0S6RN/ti0rvP8xhrQ2Yk4VcGTqEB3ptRC3xLXeNe70FXOfHrHUF4L0AdD3pzBKTc/f5Lpc2Eon5STumY7DqUQ9UU3R3MmGlwEevcUAtTSzeB8e7FQJ7Rmgo0Oflk9bO+OlJ/oXo1D007nXJoLRyXISkYXvHpgrCQ/lLF9gGvKqCz8ns0UKZRb5o1oT2XoJT/byUspk5T0BqOi9P2yyayXUWxuzDQt5F55wWZzCFE6bdfCbMDINJlZc0R4+ctorm7ppNMOCON2PHO0opedRZxuaVI6CugAvyhDxS6QegwImrfmv8BO/t4q+lu4dp1m4j5ih9MRW+VM9ygFHB/74e4opnzfMGMB7KpDeJLKn6x2MP72qpOc)
-;;;; Version 2:
-;;;; - Why use UIOP:READ-FILE-LINES while we could have just used UIOP:READ-FILE-FORMS instead?
-;;;; - We can use MEMBER instead ECASE + EQ inside WIN?
-;;;; - Don't need WINS-TO anymore: (LOSES-TO x) == (WINS-TO (WINS-TO X))
-;;;; - [link](https://topaz.github.io/paste/#XQAAAQDSBAAAAAAAAAAd6AQKBhOR2ycIlwrqRiHywFJuxueW2QjzG+6/dvsDjKKBs/yajlDlKwdE3HZ5XqEwKjWDdug73rNDtcLpzyh6ug82TWELItKFzcd6gPKBvzLPvzLOyhqXYpuMf3KAS5svwDw5oc3VqivaMfvIWrkaBmAwLNKvmL0dip556ixOS7ZHbNz1cCJJpEX60aJ9yuSwvZ8JizlFe3ciiGcaypwBztqAT4r+0+bgYuspC+TAXhpXIZGFqx6mXLwClGYOdZjXFM8JgMVBP70zVurOUKd3hd7/E0KYxJpjowt+ZKi1PqU7p51t5TBxCGKmQGuNvBq9R2xCA1Ozjtn9Q4plKMYuRxVPmWI9ITFmlmyoQGxWfWGdJLeVsFKOYsel9shmWxZ6WMLblL0WSrmMPv2TWJ/PIkdFSqpTUV5DgihXVcf6qpU+nlaWAhv2+UhFnuJA8WtB1y+b9n2hjVJpGTo7BtUv2DEt+W9cXQukm2hb61lgJ41nTTRlU6QCvhe+2F1rEJTOwH5m0WfXfq55if3veCnjb0C9Mnzb5nATHoxhgp+r4fcgX4uHUkQFWfsQEOSD6oEi7hT13zeggWWVaXe1JgEkltBfjMUOXLz/7w6Bvg==)
-
 (defpackage :aoc/2022/02 #.cl-user::*aoc-use*)
 (in-package :aoc/2022/02)
 
@@ -48,5 +34,18 @@
   (values (solve (parse-moves))
           (solve (parse-moves2))))
 
-
 (define-test (2022 02) (14297 10498))
+
+;;;; Version 2 (this):
+;;;; - Why use UIOP:READ-FILE-LINES while we could have just used UIOP:READ-FILE-FORMS instead?
+;;;; - We can use MEMBER instead ECASE + EQ inside WIN?
+;;;; - Don't need WINS-TO anymore: (LOSES-TO x) == (WINS-TO (WINS-TO X))
+;;;; - [link](https://topaz.github.io/paste/#XQAAAQDSBAAAAAAAAAAd6AQKBhOR2ycIlwrqRiHywFJuxueW2QjzG+6/dvsDjKKBs/yajlDlKwdE3HZ5XqEwKjWDdug73rNDtcLpzyh6ug82TWELItKFzcd6gPKBvzLPvzLOyhqXYpuMf3KAS5svwDw5oc3VqivaMfvIWrkaBmAwLNKvmL0dip556ixOS7ZHbNz1cCJJpEX60aJ9yuSwvZ8JizlFe3ciiGcaypwBztqAT4r+0+bgYuspC+TAXhpXIZGFqx6mXLwClGYOdZjXFM8JgMVBP70zVurOUKd3hd7/E0KYxJpjowt+ZKi1PqU7p51t5TBxCGKmQGuNvBq9R2xCA1Ozjtn9Q4plKMYuRxVPmWI9ITFmlmyoQGxWfWGdJLeVsFKOYsel9shmWxZ6WMLblL0WSrmMPv2TWJ/PIkdFSqpTUV5DgihXVcf6qpU+nlaWAhv2+UhFnuJA8WtB1y+b9n2hjVJpGTo7BtUv2DEt+W9cXQukm2hb61lgJ41nTTRlU6QCvhe+2F1rEJTOwH5m0WfXfq55if3veCnjb0C9Mnzb5nATHoxhgp+r4fcgX4uHUkQFWfsQEOSD6oEi7hT13zeggWWVaXe1JgEkltBfjMUOXLz/7w6Bvg==)
+;;;; Version 1:
+;;;; - Kept the looping logic the same, and only tweaked the parsing logic
+;;;; - Only _your_ socre is required, and if we don't have to store two scores anymore, then the LOOP form can be simplified a lot
+;;;; - [link](https://topaz.github.io/paste/#XQAAAQAkBAAAAAAAAAAd6AQKBhOR2ycIlwrqRiHywFJuxueW2QjzG+6/cZGkvU+2/1gn5WaRbwV6tIqysLuUZax8ticZmlBd5xQecpRPBgOiFTBwXnNx0FVsYvqVRQ5yHmUjD/PhdNzwUAvmypXUDyFwhRYfDmFjQ0ZCGKfHwzj0S6RN/ti0rvP8xhrQ2Yk4VcGTqEB3ptRC3xLXeNe70FXOfHrHUF4L0AdD3pzBKTc/f5Lpc2Eon5STumY7DqUQ9UU3R3MmGlwEevcUAtTSzeB8e7FQJ7Rmgo0Oflk9bO+OlJ/oXo1D007nXJoLRyXISkYXvHpgrCQ/lLF9gGvKqCz8ns0UKZRb5o1oT2XoJT/byUspk5T0BqOi9P2yyayXUWxuzDQt5F55wWZzCFE6bdfCbMDINJlZc0R4+ctorm7ppNMOCON2PHO0opedRZxuaVI6CugAvyhDxS6QegwImrfmv8BO/t4q+lu4dp1m4j5ih9MRW+VM9ygFHB/74e4opnzfMGMB7KpDeJLKn6x2MP72qpOc)
+;;;; Version 0:
+;;;; - Kept track not only of our score, but also the opponent's one (it was not required...)
+;;;; - For part 2, I ended up changing both the parsing and the looping logic (it's not required...see later)
+;;;; - [link](https://topaz.github.io/paste/#XQAAAQBWBwAAAAAAAAAd6AQKBhOR2ycIlwrqRiHywFJuxueW2QjzG+6/cZGkvU+2/1gn5WaRbwV6tIqysLuUZax8ticZmlBd5xQecpRPBgOiFTBwXnNx0FVsYvqVRQ5yHmUjD/PhdNzwUAvmypXUDyFwhRYfDmFjQ0ZCGKfHwzj0S6RN/ti0rvP8xhrQ2Yk4VcGTqEB3ptRC3xLXeNe70FXOfHrHUF4L0AdD3pzBKTc/f5Lpc2Eon5STrjYwZeSVvFjZWx991TlXDi4D/P+Cn67h2su3Ii0NwQZZhOXV4kvcZLt3Ao2Q3266mQhMEHFA4DN9259VMmIeVvmkBZ9jOEmje5M5/p2kMrbt6APSDAgVm/D78qBJ0p38XTTwqHTxVLuFJqJWMskOgdLXnPE66w7FtMBTOIGBpFF2n7ctMoQTkkccoMsZsLnOZkNDtCBgI9r0TDgtRzhc6l4uadx6hJsy5M7CVwy+G0c4pr3pIxjQ0/MbPqLudzBrdK8P+8qS41PEi83gucp3u0gw6EY6ZI86CKnYLFhEIegwYJ+h11tHh+YMXX1yOMi0qKgUGTLGvW06NvSPxkvnYeSgyEN+lpsesv1suLXjvkilDXyj/UW+dBKN7oNSWv4acuk=)
--- src/2022/day04.lisp
+++ src/2022/day04.lisp
@@ -15,10 +15,10 @@
 
 (define-test (2022 04) (569 936))
 
-;;; Version 1:
+;;; Version 1 (this):
 ;;; - Use CL-PPCRE:ALL-MATCHES-AS-STRINGS instead of CL-PPCRE:DO-REGISTER-GROUPS 
 ;;; - Merge the two loops into one
 ;;; - [link](https://topaz.github.io/paste/#XQAAAQCyAQAAAAAAAAAUGQimgx+p6NeA2AwBIArz/HBdclF3hRZev4uqus4U7xlOrEvMl4zyK5yvj0mMZbeOJYzrcga5Kk29BYV+lPdzxbKIFcr4NY9yIYA9we1UpxHa68LWFh8mseWT2JLkh3vr0bX67m2azRbZOWBB6Nv0yJipAr6tBmPVZprgTWD2lWrFLiwBNJWGhpHCwpAyohfbD4lrsrTy78vaNQtBJ2O0iTMDyurx1PDAk1DSsvU/qBOyI/ixwOR29ALr2cHiJbTRp1DjgfKP6m5PwkJwxCbuPsB9VYBsNTiYRJbfsVAjoXb692uBYJ1b8H8BpXCvn3T7FLRcFtAr+/LxH45wZT7eG/2Ht0Q=)
-;;; Version 0 (the one used for the stars):
+;;; Version 0:
 ;;; - Not to bad as is
 ;;; - [link](https://topaz.github.io/paste/#XQAAAQCfAQAAAAAAAAAUG0gnM31Tanr+2XFUdVjkXruVgqGfmNzdDUKUWZvxIR+lgjFaJ6g2/w2R0MAf8LAHR5xMDghoNC2Zp3es/poocUNnuLp0bTLGRNAUXu59t4kczMp6IJweAv3LifI54nv6s1WpgmiHOuNvlVObd1DjW9KvRNviJOif39zgY8kWV0YillelgZoXpWV3lBv0z3VZc+SnojLxw/4gP03+M4/UiEcMGz5fWPfwSvXK9x672VsaVkPEq5tMPP9jwxnzVoP7IHeTlyzPDWhm02wfzGK1OdyuU///ortKoA==)
