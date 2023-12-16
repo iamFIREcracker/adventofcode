edc0cab27fd6bc8f78c4763e27489761abc979c9
Matteo Landi <matteo@matteolandi.net>
2023-12-16 12:19:27 +0530

    Refactor 2023/13
    Simplify solutions -- we should only focus on the the numbers of
    character off: when zero, it means we found our reflection line; when 1,
    it means we found the reflection line after the smudge got fixed

--- src/2023/day13.lisp
+++ src/2023/day13.lisp
@@ -5,7 +5,8 @@
 (defun parse-input (&optional (strings (uiop:read-file-lines #P"src/2023/day13.txt")))
   (split-sequence:split-sequence "" strings :test 'string=))
 
-(defun find-reflections (pattern)
+
+(defun find-reflection (pattern &optional with-smudge?)
   (bnd* ((rows (length pattern))
          (cols (length (first pattern))))
     (flet ((row (i j1 j2)
@@ -16,111 +17,40 @@
              (looping
                (dorangei (i i1 i2)
                  (collect! (aref (nth i pattern) j))))))
-      (list
-        ;; Vertical reflections
-        (position-if (lambda (j)
-                       (every (lambda (i)
-                                (every #'char=
-                                       (reverse (row i 0 j))
-                                       (row i (1+ j) (1- cols))))
-                              (iota rows)) )
-                     (iota (1- cols)))
-        ;; Horizontal reflections
-        (position-if (lambda (i)
-                       (every (lambda (j)
-                                (every #'char=
-                                       (reverse (col j 0 i))
-                                       (col j (1+ i) (1- rows))))
-                              (iota cols)) )
-                     (iota (1- rows)))))))
-
-(defun summary (pattern)
-  (destructuring-bind (col row) (find-reflections pattern)
-    (+ (if col (1+ col) 0)
-       (* (if row (1+ row) 0) 100))))
-
-#+#:excluded (mapcar #'find-reflections (parse-input))
-#+#:excluded (reduce #'+ (parse-input) :key #'summary)
-
-(defun find-reflection-lines (pattern)
-  (append (find-vertical-reflection-lines pattern)
-          (find-horizontal-reflection-lines pattern)))
-
-(defun find-vertical-reflection-lines (pattern)
-  (bnd* ((rows (length pattern))
-         (cols (length (first pattern))))
-    (flet ((row (i j1 j2)
-             (looping
-               (dorangei (j j1 j2)
-                 (collect! (aref (nth i pattern) j)))))
-           (col (j i1 i2)
-             (looping
-               (dorangei (i i1 i2)
-                 (collect! (aref (nth i pattern) j))))))
-      (looping
-        (dorange (j 0 (1- cols))
-          (when (every (lambda (i)
-                         (every #'char=
-                                (reverse (row i 0 j))
-                                (row i (1+ j) (1- cols))))
-                       (iota rows))
-            (collect! (list :v j))))))))
-
-(defun find-horizontal-reflection-lines (pattern)
-  (bnd* ((rows (length pattern))
-         (cols (length (first pattern))))
-    (flet ((row (i j1 j2)
-             (looping
-               (dorangei (j j1 j2)
-                 (collect! (aref (nth i pattern) j)))))
-           (col (j i1 i2)
-             (looping
-               (dorangei (i i1 i2)
-                 (collect! (aref (nth i pattern) j))))))
-      (looping
-        (dorange (i 0 (1- rows))
-          (when (every (lambda (j)
-                         (every #'char=
-                                (reverse (col j 0 i))
-                                (col j (1+ i) (1- rows))))
-                       (iota cols))
-            (collect! (list :h i))))))))
-
-(defun summary (pattern)
-  (looping
-    (dolist+ ((dir val) (find-reflection-lines pattern))
-      (incf val)
-      (sum! (ecase dir (:v val) (:h (* val 100)))))))
-
-(reduce #'+ (parse-input) :key #'summary)
-
-(defun fix-smudge (pattern &aux (pattern (copy-seq pattern)))
-  (bnd* ((rows (length pattern))
-         (cols (length (first pattern)))
-         ((old-line) (find-reflection-lines pattern)))
-    (dorange (i 0 rows)
-      (dorange (j 0 cols)
-        (bnd1 (old (aref (nth i pattern) j))
-          (when (char/= old #\.)
-            (setf (aref (nth i pattern) j) #\.)
-            (dolist+ (new-line (find-reflection-lines pattern))
-              (when (not (equal old-line new-line))
-                (return-from fix-smudge new-line))))
-          (when (char/= old #\# )
-            (setf (aref (nth i pattern) j) #\# )
-            (dolist+ (new-line (find-reflection-lines pattern))
-              (when (not (equal old-line new-line))
-                (return-from fix-smudge new-line))))
-          (setf (aref (nth i pattern) j) old) )))))
+      ;; Vertical reflections
+      (dorange (j 0 (1- cols))
+        (bnd1 (diff 0)
+          (dorange (i 0 rows)
+            (dolists ((ch1 (reverse (row i 0 j)))
+                      (ch2 (row i (1+ j) (1- cols))))
+              (when (char/= ch1 ch2)
+                (incf diff))))
+          ;; When there is a smudge only one character along the reflection
+          ;; line will be off; also, there can only be one smudge per pattern,
+          ;; so once we found a reflection with only one character off, there
+          ;; we have found our reflection line once the smudge was fixed.
+          (when (= diff (if with-smudge? 1 0))
+            (return-from find-reflection (list :v j)))))
+      ;; Horizontal reflections
+      (dorange (i 0 (1- rows))
+        (bnd1 (diff 0)
+          (dorange (j 0 cols)
+            (dolists ((ch1 (reverse (col j 0 i)))
+                      (ch2 (col j (1+ i) (1- rows))))
+              (when (char/= ch1 ch2)
+                (incf diff))))
+          (when (= diff (if with-smudge? 1 0))
+            (return-from find-reflection (list :h i))))))))
 
 
 (defun summary (reflection-line)
   (destructuring-bind (dir val) reflection-line
-    (incf val)
-    (ecase dir (:v val) (:h (* val 100)))))
-(mapcar #'fix-smudge (parse-input))
-(reduce #'+ * :key #'summary)
+    (ecase dir
+      (:v (1+ val))
+      (:h (* (1+ val) 100)))))
 
-; can there be different reflection lines?
-; can there 
+(define-solution (2023 13) (input parse-input)
+  (values (reduce #'+ (mapcar #'find-reflection input) :key #'summary)
+          (reduce #'+ (mapcar [find-reflection _ t] input) :key #'summary)))
 
+(define-test (2023 13) (29846 25401))
