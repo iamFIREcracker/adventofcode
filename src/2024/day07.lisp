f1afeb91ca3d1a83ad852ffce067c85a0b77ddfc
Matteo Landi <matteo@matteolandi.net>
2024-12-07 07:15:36 +0100

    Refactor + input for day7

--- src/2024/day07.lisp
+++ src/2024/day07.lisp
@@ -1,18 +1,13 @@
 (defpackage :aoc/2024/07 #.cl-user::*aoc-use*)
 (in-package :aoc/2024/07)
 
-#;
 (defun parse-equations (&optional (strings (uiop:read-file-lines #P"src/2024/day07.txt")))
   (mapcar #'extract-positive-integers strings))
 #+#:excluded (parse-equations)
 
-(defun || (a b)
-  (parse-integer (spr a b)))
-
-(defun solvable? (test numbers &optional (operators '(+ * ||)))
+(defun solvable? (test numbers &optional (operators '(+ *)))
   (recursively ((test test) (numbers numbers))
     (cond ((= (length numbers) 1) (= (first numbers) test))
-          ((minusp test) nil)
           (t (destructuring-bind (a b . numbers1) numbers
                (looping
                  (dolist (op operators)
@@ -20,7 +15,19 @@
                      (thereis! (recur test
                                       (cons x numbers1)))))))))))
 
-(looping
-  (doseq ((test . numbers) (parse-equations))
-    (when (solvable? test numbers)
-      (sum! test))))
+
+(defun || (a b)
+  (parse-integer (spr a b)))
+
+
+(define-solution (2024 7) (input parse-equations)
+  (values (looping
+            (doseq ((test . numbers) input)
+              (when (solvable? test numbers)
+                (sum! test))))
+          (looping
+            (doseq ((test . numbers) input)
+              (when (solvable? test numbers '(+ * ||))
+                (sum! test))))))
+
+(define-test (2024 07) (1298300076754 248427118972289))
