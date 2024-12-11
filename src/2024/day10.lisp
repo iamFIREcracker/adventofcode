118a017a17028e9d203129fe6f58d53189e6724f
Matteo Landi <matteo@matteolandi.net>
2024-12-11 21:51:04 +0100

    Refactor 2024/10

--- src/2024/day10.lisp
+++ src/2024/day10.lisp
@@ -1,42 +1,37 @@
 (defpackage :aoc/2024/10 #.cl-user::*aoc-use*)
 (in-package :aoc/2024/10)
 
-#;
+
 (defun parse-input (&optional (strings (uiop:read-file-lines #P"src/2024/day10.txt")))
   (prog1-let (map (make-hash-table :test 'equal))
     (doseq ((i s) (enumerate strings))
       (doseq ((j ch) (enumerate s))
-        (setf (@ map (list i j)) (- (char-code ch) (char-code #\0)))))))
+        (setf (gethash (list i j) map) (- (char-code ch) (char-code #\0)))))))
 #+#:excluded (parse-input)
 
-(defun count-trailheads (map i j)
-  (let1 frontier (list (list i j))
-    (length
-      (remove-duplicates
-        (looping
-          (while frontier
-            (destructuring-bind (i j) (pop frontier)
-              (when (= (@ map (list i j)) 9)
-                (collect! (list i j)))
-              (doseq ((di dj) '((-1 0) (0 1) (1 0) (0 -1)))
-                (let ((i1 (+ i di)) (j1 (+ j dj)))
-                  (if (aand (@ map (list i1 j1)) (= (1- it) (@ map (list i j))))
-                      (push (list i1 j1) frontier)))))))
-        :test 'equal))))
-(defun count-trailheads (map i j)
+
+(defun trailheads (map i j)
   (let1 frontier (list (list i j))
     (looping
       (while frontier
         (destructuring-bind (i j) (pop frontier)
-          (count! (= (@ map (list i j)) 9))
-          (doseq ((di dj) '((-1 0) (0 1) (1 0) (0 -1)))
-            (let ((i1 (+ i di)) (j1 (+ j dj)))
-              (if (aand (@ map (list i1 j1)) (= (1- it) (@ map (list i j))))
-                  (push (list i1 j1) frontier)))))))))
-(let1 map (parse-input)
-  (looping
-    (dohash ((i j) height map)
-      (when (= height 0)
-        (sum! (count-trailheads map i j))))))
-969 nope
-(untrace count-trailheads)
+          (if (= (gethash (list i j) map) 9)
+              (collect! (list i j))
+              (doseq ((di dj) '((-1 0) (0 1) (1 0) (0 -1)))
+                (let ((i1 (+ i di)) (j1 (+ j dj)))
+                  (if (aand (gethash (list i1 j1) map) (= (1- it) (gethash (list i j) map)))
+                      (push (list i1 j1) frontier))))))))))
+
+
+(define-solution (2024 10) (map parse-input)
+  (values
+    (looping
+      (dohash ((i j) height map)
+        (when (= height 0)
+          (sum! (length (remove-duplicates (trailheads map i j) :test 'equal))))))
+    (looping
+      (dohash ((i j) height map)
+        (when (= height 0)
+          (sum! (length (trailheads map i j))))))))
+
+(define-test (2024 10) (472 969))
