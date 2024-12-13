17025bc046ca873228f537cb1f229236f66e003b
Matteo Landi <matteo@matteolandi.net>
2024-12-13 14:18:33 +0100

    Clean up 2024/13 and add input

--- src/2024/day13.lisp
+++ src/2024/day13.lisp
@@ -1,54 +1,12 @@
 (defpackage :aoc/2024/13 #.cl-user::*aoc-use*)
 (in-package :aoc/2024/13)
 
-#;
-(sb-ext:gc :full t)
 (defun parse-input (&optional (strings (uiop:read-file-lines #P"src/2024/day13.txt")))
   (looping
     (doseq (configuration (split-sequence:split-sequence "" strings :test 'equal))
      (collect! (mapcar #'extract-positive-integers configuration)))))
-#+#:excluded (parse-input)
 
-(defun move-straight (pos dir) (mapcar #'+ pos dir))
-(defun overshoot? (curr target)
-  (or (> (car curr) (car target))
-      (> (cadr curr) (cadr target))))
 
-(defun minimize-presses (a b prize &optional (start (list 0 0)))
-  (a* start
-      :goal-state prize :test 'equal
-      :neighbors (fn (curr)
-                   (looping
-                     (doseqs ((dir (list a b))
-                              (cost (list 3 1)))
-                       (let1 next (move-straight curr dir)
-                         (unless (overshoot? next prize)
-                           (collect! (cons next cost)))))))
-      :heuristic [manhattan-distance _ prize]))
-
-(looping
-  (doseq ((a b prize) (parse-input))
-    (awhen (search-cost (minimize-presses a b prize))
-      (sum! it))))
-39290!
-
-;; bruteforce
-(defun solve (a b price)
-  (destructuring-bind (ax ay) a
-    (destructuring-bind (bx by) b
-      (destructuring-bind (px py) price
-        (looping
-          (dorangei (n 0 100)
-            (dorangei (m 0 100)
-              (when (and (= (+ (* ax n) (* bx m)) px)
-                         (= (+ (* ay n) (* by m)) py))
-                (minimize! (+ (* n 3) m))))))))))
-(looping
-  (doseq ((a b prize) (parse-input))
-    (awhen (solve a b prize)
-      (sum! it))))
-
-;; Math
 (defun solve (a b price)
   (destructuring-bind (ax ay) a
     (destructuring-bind (bx by) b
@@ -61,15 +19,17 @@
                      ax)
             (if (and (integerp m) (integerp n))
                 (+ (* n 3) m))))))))
-(assert
-  (= (looping
-       (doseq ((a b prize) (parse-input))
-         (awhen (solve a b prize)
-           (sum! it))))
-     39290))
 
-(looping
-  (doseq ((a b (px py)) (parse-input))
-    (awhen (solve a b (list (+ px 10000000000000) (+ py 10000000000000)))
-      (sum! it))))
-73458657399094!
+
+
+(define-solution (2024 13) (input parse-input)
+  (values (looping
+            (doseq ((a b prize) input)
+              (awhen (solve a b prize)
+                (sum! it))))
+          (looping
+            (doseq ((a b (px py)) input)
+              (awhen (solve a b (list (+ px 10000000000000) (+ py 10000000000000)))
+                (sum! it))))))
+
+(define-test (2024 13) (39290 73458657399094))
