774e939ffba19c60cab3f1b37dea7b86994bffb4
Matteo Landi <matteo@matteolandi.net>
2024-12-13 14:19:07 +0100

    Clean up 2024/12 and add input

--- src/2024/day12.lisp
+++ src/2024/day12.lisp
@@ -1,8 +1,6 @@
 (defpackage :aoc/2024/12 #.cl-user::*aoc-use*)
 (in-package :aoc/2024/12)
 
-#;
-
 (defun parse-input (&optional (strings (uiop:read-file-lines #P"src/2024/day12.txt")))
   (prog1-let (map (make-hash-table :test 'equal))
     (doseq ((i s) (enumerate strings))
@@ -10,93 +8,50 @@
         (setf (gethash (list i j) map) ch)))))
 #+#:excluded (parse-input)
 
-#+#:overall-area-and-perimeter-instead-of-by-region (let1 map (parse-input)
-                                                      (let ((areas (make-hash-table))
-                                                            (perimeters (make-hash-table)))
-                                                        (dohash ((i j) type map)
-                                                          (incf (gethash type areas 0))
-                                                          (doseq ((di dj) '((-1 0) (0 1) (1 0) (0 -1)))
-                                                            (when-not (eql (gethash (list (+ i di) (+ j dj)) map) type)
-                                                              (incf (gethash type perimeters 0)))))
-                                                        (looping
-                                                          (dohash (type area areas)
-                                                            (let1 perimeter (gethash type perimeters)
-                                                              (dbgl type area perimeter (* area perimeter))
-                                                              (sum! (* area perimeter)))))))
-13799638 too high
 
-(let1 map (parse-input)
-  (let1 seen (make-hash-table :test 'equal)
-    (let1 regions (looping
-                    (dohash ((i j) type map)
-                      (let ((area 0) (perimeter 0)
-                            (frontier (list (list i j))))
-                        (while frontier
-                          (destructuring-bind (i j) (pop frontier)
-                            (unless (gethash (list i j) seen)
-                              (setf (gethash (list i j) seen) t)
-                              (incf area)
-                              (doseq ((di dj) '((-1 0) (0 1) (1 0) (0 -1)))
-                                (let1 next (list (+ i di) (+ j dj))
-                                  (if (eql (gethash next map) type)
-                                      (push next frontier)
-                                      (incf perimeter)))))))
-                        (when (and (plusp area) (plusp perimeter))
-                          (collect! (list type area perimeter))))))
-
-      (looping
-        (doseq ((type area perimeter) regions)
-          ; (dbgl type area perimeter (* area perimeter))
-          (sum! (* area perimeter)))))))
-
-1431440!
-
-(defun rotate-cw (dir) (list (second dir) (- (first dir))))
-(defun rotate-ccw (dir) (list (- (second dir)) (first dir)))
-
-(defun sides (perimeter &aux (perimeter (copy-seq perimeter)))
-  ; (dbgl perimeter)
+(defun regions (&optional (map (parse-input)))
   (looping
-    (while perimeter
-      (destructuring-bind ((i j) dir) (pop perimeter)
-        (count! 1)
-        (doseq ((di1 dj1) (list (rotate-cw dir) (rotate-ccw dir)))
-          (let1 next (list (+ i di1) (+ j dj1))
-            ; (dbgl (list i j) dir di1 dj1 next)
+    (let1 seen (make-hash-table :test 'equal)
+      (dohash ((i j) type map)
+        (let ((area 0) fence
+              (frontier (list (list i j))))
+          (while frontier
+            (destructuring-bind (i j) (pop frontier)
+              (unless (gethash (list i j) seen)
+                (setf (gethash (list i j) seen) t)
+                (incf area)
+                (doseq ((di dj) '((-1 0) (0 1) (1 0) (0 -1)))
+                  (let1 next (list (+ i di) (+ j dj))
+                    (if (eql (gethash next map) type)
+                        (push next frontier)
+                        (push (list (list i j) (list di dj)) fence)))))))
+          (when (and (plusp area) fence)
+            (collect! (list type area fence))))))))
+(defun rtype (r) (car r))
+(defun area (r) (cadr r))
+(defun fence (r) (caddr r))
+(defun perimeter (r) (length (fence r)))
 
-            (while (find (list next dir) perimeter :test 'equal)
-              ; (break)
-              (zapf perimeter [remove (list next dir) _ :test 'equal])
-              (incf (car next) di1)
-              (incf (cadr next) dj1))))
-        ))))
-#+#:excluded (sides '(((0 3) (1 0)) ((0 3) (0 1)) ((0 3) (-1 0)) ((0 2) (1 0))
-                      ((0 2) (-1 0)) ((0 1) (1 0)) ((0 1) (-1 0)) ((0 0) (0 -1))
-                      ((0 0) (1 0)) ((0 0) (-1 0))))
 
-(let1 map (parse-input)
-  (let1 seen (make-hash-table :test 'equal)
-    (let1 regions (looping
-                    (dohash ((i j) type map)
-                      (let ((area 0) (perimeter nil)
-                            (frontier (list (list i j))))
-                        (while frontier
-                          (destructuring-bind (i j) (pop frontier)
-                            (unless (gethash (list i j) seen)
-                              (setf (gethash (list i j) seen) t)
-                              (incf area)
-                              (doseq ((di dj) '((-1 0) (0 1) (1 0) (0 -1)))
-                                (let ((next (list (+ i di) (+ j dj))))
-                                  (if (eql (gethash next map) type)
-                                      (push next frontier)
-                                      (push (list (list i j) (list di dj)) perimeter)))))))
-                        (when (and (plusp area) (length perimeter))
-                          (collect! (list type area perimeter))))))
+;; Each straight section of fence counts as a side, regardless of how long it
+;; is.  So for each section, we can check if there is another one adjacent,
+;; facing the same direction, and if there is, we subtract one from the total
+;; sides count.
+(defun sides (r &aux (fence (fence r)))
+  (prog1-let (total (length fence))
+    (flet ((adjacent? (p1 p2)
+             (= (+ (abs (- (car p1) (car p2)))
+                   (abs (- (cadr p1) (cadr p2))))
+                1)))
+      (dosublists (((pos dir) . remaining) fence)
+        (doseq ((pos1 dir1) remaining)
+          (when (and (equal dir dir1) (adjacent? pos pos1))
+            (decf total)))))))
 
-      (looping
-        (doseq ((type area perimeter) regions)
-          (let1 sides (sides perimeter)
-            ; (dbgl type area perimeter sides)
-            (sum! (* area sides))))))))
-1411840 too high
-869070!
+
+(define-solution (2024 12) (map parse-input)
+  (let1 regions (regions map)
+    (values (reduce #'+ regions :key [* (area _)  (perimeter _)])
+            (reduce #'+ regions :key [* (area _)  (sides _)]) )))
+
+(define-test (2024 12) (1431440 869070))
