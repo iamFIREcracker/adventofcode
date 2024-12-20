db115cd7a8d1fe82f07e55baa93d2a3be17905e3
Matteo Landi <matteo@matteolandi.net>
2024-12-20 18:09:12 +0100

    Refactor 2024/19

--- src/2024/day19.lisp
+++ src/2024/day19.lisp
@@ -1,37 +1,28 @@
 (defpackage :aoc/2024/19 #.cl-user::*aoc-use*)
 (in-package :aoc/2024/19)
 
-#;
 (defun parse-input (&optional (strings (uiop:read-file-lines #P"src/2024/day19.txt")))
   (destructuring-bind ((patterns) designs) (split-sequence:split-sequence "" strings :test 'string=)
     (list (cl-ppcre:split ", " patterns)
           designs)))
 #+#:excluded (parse-input)
 
-(destructuring-bind (patterns designs) (parse-input)
-  (looping
-    (dolist (design designs)
-      (let1 memo (make-hash-table :test 'equal)
-        (labels ((recur (patterns design)
-                   (memoizing (memo patterns design)
-                     (cond ((zerop (length design)) t)
-                           ((null patterns) nil)
-                           (t (looping
-                                (dolist (p patterns)
-                                  (when (string-starts-with-p p design)
-                                    (thereis! (recur patterns (subseq design (length p))))))))))))
-          (count! (recur patterns design)))))))
 
-(destructuring-bind (patterns designs) (parse-input)
-  (looping
-    (dolist (design designs)
-      (let1 memo (make-hash-table :test 'equal)
-        (labels ((recur (patterns design)
-                   (memoizing (memo patterns design)
-                     (cond ((zerop (length design)) 1)
-                           ((null patterns) 0)
-                           (t (looping
-                                (dolist (p patterns)
-                                  (when (string-starts-with-p p design)
-                                    (sum! (recur patterns (subseq design (length p))))))))))))
-          (sum! (recur patterns design)))))))
+(defun design-possible? (patterns design)
+  (let1 memo (make-hash-table :test 'equal)
+    (recursively ((start 0))
+      (memoizing (memo start)
+        (cond ((= (length design) start) 1)
+              ((null patterns) 0)
+              (t (looping
+                   (dolist (p patterns)
+                     (when (string-starts-with-p p design :start start)
+                       (sum! (recur (+ (length p) start))))))))))))
+
+
+(define-solution (2024 19) (input parse-input)
+  (destructuring-bind (patterns designs) input
+    (values (count-if #'plusp designs :key [design-possible? patterns _])
+            (reduce #'+ designs :key [design-possible? patterns _]))))
+
+(define-test (2024 19) (350 769668867512623))
