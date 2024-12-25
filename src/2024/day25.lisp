f363f54f5d56d558a5ac1a7462a1945b763e7a19
Matteo Landi <matteo@matteolandi.net>
2024-12-25 06:38:13 +0100

    Clean up 2024/25

--- src/2024/day25.lisp
+++ src/2024/day25.lisp
@@ -1,33 +1,38 @@
 (defpackage :aoc/2024/25 #.cl-user::*aoc-use*)
 (in-package :aoc/2024/25)
 
-#;
-
-
 (defun parse-input (&optional (strings (uiop:read-file-lines #P"src/2024/day25.txt")))
   (let (locks keys)
     (let1 blocks (split-sequence:split-sequence "" strings :test 'equal)
-      (dolist (strings blocks)
-        (let1 whatever (list -1 -1 -1 -1 -1)
-          (doeseq (i s strings)
-            (doeseq (j ch s)
-              (if (char= ch #\#)
-                  (incf (nth j whatever)))))
-          (if (char= (char (nth 0 strings) 0) #\#)
-              (push whatever locks)
-              (push whatever keys)) )))
+      (dolist (block blocks)
+        (if (char= (char (nth 0 block) 0) #\#)
+            (push block locks)
+            (push block keys))))
     (list locks keys)))
 #+#:excluded (parse-input)
 
-(defun good (x1 x2)
-  (looping
-    (dolists ((xx1 x1)
-              (xx2 x2))
-      (always! (<= (+ xx1 xx2) 5)))))
 
-(destructuring-bind (locks keys) (parse-input)
-  (dbgl locks keys)
+;; I originally went on and converted each block into an array of numbers
+;; (representing the height of each pin / hole); then, to solve the problem,
+;; for each pair I would check and confirm that the sum the heights for each
+;; column was never greater than 5.
+;; Turns out there is a simpler approach: for each position in the block,
+;; make sure both lock and keys have a # in it, as that would mean the two
+;; would overlap.  Slower, for sure, but easier to relate to.
+(defun fit? (block1 block2)
   (looping
-    (dolist (l locks)
-      (dolist (k keys)
-        (count! (good l k))))))
+    (dolists ((row1 block1)
+              (row2 block2))
+      (doseqs ((ch1 row1)
+               (ch2 row2))
+        (never! (char= ch1 ch2 #\#))))))
+
+
+(define-solution (2024 25) (input parse-input)
+  (destructuring-bind (locks keys) input
+    (looping
+      (dolist (l locks)
+        (dolist (k keys)
+          (count! (fit? l k)))))))
+
+(define-test (2024 25) (2978))
