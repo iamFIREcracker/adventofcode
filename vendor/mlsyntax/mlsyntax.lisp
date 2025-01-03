22574c04075015da718c94a5fe215df7bbc9eb57
Matteo Landi <matteo@matteolandi.net>
2025-01-03 15:24:23 +0100

    Refactor and add input for 2024/22
    Note: in my original solution I was iterating over the list of winning
    prices over and over, for each buyer; it goes without saying it how long
    it would take for it to spit the right answer.  Well, turns out you can
    just iterate each winning price per sequence per buyer once, and then
    spit out the _max_ one.

--- vendor/mlsyntax/mlsyntax.lisp
+++ vendor/mlsyntax/mlsyntax.lisp
@@ -30,25 +30,37 @@ Examples:
          (declare (ignorable ,arg0% ,arg0_))
          ,body))))
 
-(defun tilde-reader (stream sub-char numarg)
-  "#~foo.bar => (bar foo)"
-  (declare (ignore sub-char numarg))
-  (let ((following (read stream)))
-    (if (position #\. (string following))
-        (let ((parts (split-sequence:split-sequence #\. (string following))))
-          (reduce (lambda (acc cur) (list (intern cur) acc)) (cdr parts) :initial-value (intern (car parts))))
-        following)))
+(defun read-until-dot (stream)
+  (with-output-to-string (s)
+    (loop for char = (peek-char nil stream nil nil)
+          while (and char (not (find char " .)]")))
+          do (write-char (read-char stream) s))))
+#+#:excluded (with-input-from-string (s "foo.bar") (read-until-dot s))
+#+#:excluded (with-input-from-string (s "foo bar") (read-until-dot s))
+#+#:excluded (with-input-from-string (s "foo)") (read-until-dot s))
+#+#:excluded (with-input-from-string (s "foo]") (read-until-dot s))
+#+#:excluded (with-input-from-string (s "foo") (read-until-dot s))
 
-#+#:excluded (with-input-from-string (s "foo.car")
-               (tilde-reader s #\~))
-#+#:excluded (with-input-from-string (s "(list :~>)")
-               (tilde-reader s #\~))
+(defun parse-dot-chain (stream char)
+  ;; Only process if next char is a valid symbol start
+  (when (not (alphanumericp (peek-char nil stream nil nil)))
+    (let ((*readtable* (copy-readtable)))
+      (set-macro-character #\~ nil)
+      (unread-char char stream)
+      (return-from parse-dot-chain (values (read stream t nil t)))))
+
+  (let* ((first-token (read-from-string (read-until-dot stream)))
+         (result first-token))
+    (loop while (eql (peek-char nil stream nil nil) #\.)
+          do (read-char stream) ; consume the dot
+          do (let ((next-token (read-from-string (read-until-dot stream))))
+               (setf result (list next-token result))))
+    result))
 
 (defun sharp-semicolon-reader (stream sub-char numarg)
   (declare (ignore sub-char numarg))
   (loop :while (read-line stream nil nil))
   (values))
-
 
 #+#:excluded (get-dispatch-macro-character #\# #\~)
 ;; https://stackoverflow.com/questions/30699851/what-does-the-non-terminating-p-argument-of-set-macro-character-do
@@ -58,7 +70,7 @@ Examples:
   ;; keeps on working as expected
   (:macro-char #\[ #'opening-braket-reader t)
   (:macro-char #\] (get-macro-character (char ")" 0)))
-  (:dispatch-macro-char #\# #\~ #'tilde-reader)
+  (:macro-char #\~ 'parse-dot-chain t)
   (:dispatch-macro-char #\# #\; #'sharp-semicolon-reader)
   #+#:excluded (:macro-char #\" #'pythonic-string-reader::read-multiline-string t)
   )
