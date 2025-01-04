d0462370a561dbc47373c0c8a7478110ebef4e33
Matteo Landi <matteo@matteolandi.net>
2025-01-04 16:30:42 +0100

    Explain 2024/17 and add input

--- src/2024/day17.lisp
+++ src/2024/day17.lisp
@@ -1,96 +1,152 @@
 (defpackage :aoc/2024/17 #.cl-user::*aoc-use*)
 (in-package :aoc/2024/17)
 
-#;
-(sb-ext:gc :full t)
-
 (defun parse-input (&optional (strings (uiop:read-file-lines #P"src/2024/day17.txt")))
   (destructuring-bind (registers program) (split-sequence:split-sequence "" strings :test 'equal)
     (list (mapcan #'extract-positive-integers registers)
-          (coerce (extract-positive-integers (first program)) 'vector))))
+          (coerce (extract-positive-integers ~program.first) 'vector))))
 #+#:excluded (parse-input)
-(defaccessor a (r) (accesses (car r)))
-(defaccessor b (r) (accesses (cadr r)))
-(defaccessor c (r) (accesses (caddr r)))
 
-; (defun literal (regs rand)
-;   (declare (ignore regs))
-;   rand)
+(defaccessor a (r) (accesses ~r.car))
+(defaccessor b (r) (accesses ~r.cadr))
+(defaccessor c (r) (accesses ~r.caddr))
+
 
 (defun combo (regs rand)
   (ecase rand
     ((0 1 2 3) rand)
-    (4 (a regs))
-    (5 (b regs))
-    (6 (c regs))
+    (4 ~regs.a)
+    (5 ~regs.b)
+    (6 ~regs.c)
     (7 (error "Unused"))))
 
-(destructuring-bind (regs program) (parse-input)
-  (let1 ip 0
-    (labels ((next () (prog1 (aref program ip) (incf ip))))
-      (format nil "~{~A~^,~}"
-              (looping
-                (while (array-in-bounds-p program ip)
-                  (let ((instr (next))
-                        (rand (next)))
-                    (ecase instr
-                      (0 (setf (a regs) (floor (a regs) (expt 2 (combo regs rand)))))
-                      (1 (setf (b regs) (logxor (b regs) rand)))
-                      (2 (setf (b regs) (mod (combo regs rand) 8)))
-                      (3 (if (/= (a regs) 0) (setf ip rand)))
-                      (4 (setf (b regs) (logxor (b regs) (c regs))))
-                      (5 (collect! (mod (combo regs rand) 8)))
-                      (6 (setf (b regs) (floor (a regs) (expt 2 (combo regs rand)))))
-                      (7 (setf (c regs) (floor (a regs) (expt 2 (combo regs rand))))) ))))))))
-6,1,6,4,2,4,7,3,5
-
-(destructuring-bind (regs program) (parse-input)
-  (let1 ip 0
-    (setf (a regs) 'x)
-    (labels ((next () (prog1 (aref program ip) (incf ip))))
-      (format nil "~{~A~^,~}"
-              (looping
-                (let1 outd 0
-                  (while (and (array-in-bounds-p program ip)
-                              (< outd (length program)))
+(defun run (&optional (input (parse-input)))
+  (destructuring-bind (regs program) input
+    (let1 ip 0
+      (labels ((next () (prog1 (aref program ip) (incf ip))))
+        (format nil "~{~A~^,~}"
+                (looping
+                  (while (array-in-bounds-p program ip)
                     (let ((instr (next))
                           (rand (next)))
                       (ecase instr
-                        (0 (setf (a regs) `(floor ,(a regs) (expt 2 ,(combo regs rand)))))
-                        (1 (setf (b regs) `(logxor ,(b regs) ,rand)))
-                        (2 (setf (b regs) `(mod ,(combo regs rand) 8)))
-                        (3 (dbgl (a regs)) (setf ip 0 (a regs) 'x) (continuable (break)))
-                        (4 (setf (b regs) `(logxor ,(b regs) ,(c regs))))
-                        (5 (incf outd) (collect! (dbg `(mod ,(combo regs rand) 8))))
-                        (6 (setf (b regs) `(floor ,(a regs) (expt 2 ,(combo regs rand)))))
-                        (7 (setf (c regs) `(floor ,(a regs) (expt 2 ,(combo regs rand)))))
-                        )))))))))
+                        (0 (setf ~regs.a (floor ~regs.a (expt 2 (combo regs rand)))))
+                        (1 (setf ~regs.b (logxor ~regs.b rand)))
+                        (2 (setf ~regs.b (mod (combo regs rand) 8)))
+                        (3 (if (/= ~regs.a 0) (setf ip rand)))
+                        (4 (setf ~regs.b (logxor ~regs.b ~regs.c)))
+                        (5 (collect! (mod (combo regs rand) 8)))
+                        (6 (setf ~regs.b (floor ~regs.a (expt 2 (combo regs rand)))))
+                        (7 (setf ~regs.c (floor ~regs.a (expt 2 (combo regs rand))))) ))))))))  )
 
-; (MOD
-;  (LOGXOR (LOGXOR (LOGXOR (MOD X 8) 1) 4)
-;          (FLOOR X (EXPT 2 (LOGXOR (MOD X 8) 1))))
-;  8)
 
-; (A REGS) (FLOOR X (EXPT 2 3))
+;; By inspecting the input one could see that the program is just a plain loop,
+;; repeating the same instructions over and over again; the last instruction is
+;; usually a jnz/3, and before that you will usually find a out/5.  Letting the
+;; program run you will notice two more things: that reg B and C always get
+;; reset somewhere at the beginning of each iteration (which means we can
+;; define what out/5 will output as a function of reg A at the beginning of the
+;; iteration); second, that reg A keeps on getting reduced via adv/0.
+;;
+;; For example, if we make X the content of reg A at the beginning of each
+;; iteration, my program will out/5 the result of the following expression:
+;;
+;;     (MOD
+;;      (LOGXOR (LOGXOR (LOGXOR (MOD X 8) 1) 4)
+;;              (FLOOR X (EXPT 2 (LOGXOR (MOD X 8) 1))))
+;;      8)
+;;
+;; Moreover, thanks to adv/0, the program will keep on reducing reg A using the
+;; following expression:
+;;
+;;     (FLOOR X (EXPT 2 3))
+;;
+;; How is this useful?  Well, we need to find the initial value of reg A such
+;; that the program will output itself.  Since all that matters is the value of
+;; reg A at the beginning of each iteration, we can start from the end, and
+;; figure out which value of reg A will generate the last operand; then, by
+;; reversing the effect of adv/0, we can try to figure out which value of reg
+;; A at the second last iteration will cause out/5 to output the last opcode;
+;; then, move on to the second last operand, until we get to the first opcode.
+;; 
+;; For example: my program ends with "3,0".  If we want the last iteration to
+;; output 0, we need to find a value of reg A, X, that satisfies:
+;;
+;;     (= (MOD
+;;         (LOGXOR (LOGXOR (LOGXOR (MOD X 8) 1) 4)
+;;                 (FLOOR X (EXPT 2 (LOGXOR (MOD X 8) 1))))
+;;         8)
+;;        0) ; last operand of my program
+;;
+;; Now, we know the program should halt, which implies reg A should to be 0 at
+;; the end of the iteration; reg A gets updated via (FLOOR X (EXPT 2 3)), so if
+;; we reverse that, we will find ourselves to check all the values such that:
+;;
+;;     (= (FLOOR X (EXPT 2 3))
+;;        0) ; so the program halts
+;;
+;; This means reg A, i.e., X, at the beginning of the last iteration could
+;; only assume one of the following values: 0, 1, 2, 3, 4, 5, 6, 7.  Which of these
+;; will cause out/5 to output 0? 5!
+;;
+;; Moving on, if we want the second last iteration to output 3 we need to find
+;; X such that:
+;;
+;;     (= (MOD
+;;         (LOGXOR (LOGXOR (LOGXOR (MOD X 8) 1) 4)
+;;                 (FLOOR X (EXPT 2 (LOGXOR (MOD X 8) 1))))
+;;         8)
+;;        3) ; last opcode of my program
+;; 
+;; In this case, all the possible values of reg A to check are all the values
+;; such that:
+;;
+;;     (= (FLOOR X (EXPT 2 3))
+;;        5) ; so it will out/5 the right thing during the next iteration
+;;
+;; Or, more explicitly: 40, 41, 42, 43, 44, 45, 46, 47.
 
-(defun solves? (target x)
-  (= (mod
-       (logxor (logxor (logxor (mod x 8) 1) 4)
-               (floor x (expt 2 (logxor (mod x 8) 1))))
-       8)
-     target))
+(defun find-program-specifics (&optional (input (parse-input)))
+  (destructuring-bind (regs program) input
+    (let1 ip 0
+      (setf ~regs.a 'x)
+      (labels ((next () (prog1 (aref program ip) (incf ip))))
+        (let (out-expr adv-expr)
+          (while (array-in-bounds-p program ip)
+            (let ((instr (next))
+                  (rand (next)))
+              (ecase instr
+                (0 (setf adv-expr `(floor ,~regs.a (expt 2 ,(combo regs rand)))))
+                (1 (setf ~regs.b `(logxor ,~regs.b ,rand)))
+                (2 (setf ~regs.b `(mod ,(combo regs rand) 8)))
+                (3 :noop)
+                (4 (setf ~regs.b `(logxor ,~regs.b ,~regs.c)))
+                (5 (setf out-expr `(mod ,(combo regs rand) 8)))
+                (6 (setf ~regs.b `(floor ,~regs.a (expt 2 ,(combo regs rand)))))
+                (7 (setf ~regs.c `(floor ,~regs.a (expt 2 ,(combo regs rand))))))))
+          (values out-expr adv-expr))))))
+#+#:excluded (find-program-specifics)
 
-(destructuring-bind (regs program) (parse-input)
-  (zapf program [reverse (coerce _ 'list)])
-  (looping
-    (labels ((recur (program x)
-               (cond ((null program) (minimize! (floor x 8)))
-                     (t (if (solves? (car program) x)
-                            (dorangei (x1 (* x 8) (+ (* x 8) 7))
-                              (recur (cdr program) x1))))
-                     (t (error "NEVER")))))
-      (dolist (x (iota 8))
-        (recur program x)))))
-1623801469161855; too high
-1623801469161808; too high
-202975183645226
+
+(defun find-reg-a-for-quine (&optional (input (parse-input)) &aux (program ~input.second))
+  (multiple-value-bind (out-expr adv-expr) (find-program-specifics input)
+    (assert (equal adv-expr '(floor x (expt 2 3))))
+    (flet ((candidates (x) (iota 8 :start (* x 8))))
+      (let1 out-fn (eval `(lambda (x) ,out-expr))
+        (let1 rev-program (~> program (coerce ~ 'list) reverse)
+          (looping
+            (labels ((recur (remaining x)
+                       (cond ((null remaining) (minimize! (floor x 8)))
+                             (t (if (= (funcall out-fn x) ~remaining.car)
+                                    (dolist (x1 (candidates x))
+                                      (recur ~remaining.cdr x1)))))))
+              (dolist (x (candidates 0))
+                (recur rev-program x)))))))))
+#+#:excluded (find-reg-a-for-quine)
+
+
+(define-solution (2024 17) (input parse-input)
+  (values (run input)
+          (find-reg-a-for-quine input)))
+
+(define-test (2024 17) ("6,1,6,4,2,4,7,3,5" 202975183645226))
