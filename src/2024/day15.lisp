b44974b60da898f2896ecb8c62b51d9daa854ff5
Matteo Landi <matteo@matteolandi.net>
2025-01-06 15:48:31 +0100

    Clean up 2024/15 and add input file

--- src/2024/day15.lisp
+++ src/2024/day15.lisp
@@ -1,174 +1,113 @@
 (defpackage :aoc/2024/15 #.cl-user::*aoc-use*)
 (in-package :aoc/2024/15)
 
-#;
-(sb-ext:gc :full t)
-
-(defun move-straight (dir pos) (mapcar #'+ pos dir))
-(defun rotate-cw (dir) (list (second dir) (- (first dir))))
-#+#:excluded (defun rotate-ccw (dir) (list (- (second dir)) (first dir)))
+(defstruct (warehouse (:conc-name nil))
+  grid robot)
 
 (defun parse-input (&optional (strings (uiop:read-file-lines #P"src/2024/day15.txt")))
-  (destructuring-bind (map movements) (split-sequence:split-sequence "" strings :test 'string=)
-    (flet ((parse-map (strings)
-             (let  ((map (make-hash-table :test 'equal))
-                    (robot nil))
-               (doseq ((i s) (enumerate strings))
-                 (doseq ((j ch) (enumerate s))
-                   (setf (gethash (list i j) map) ch)
-                   (if (char= ch #\@) (setf robot (list i j)))))
-               (list map robot)))
-           (parse-movements (strings)
-             (looping
-               (dolist (s strings)
-                 (doseq (ch s)
-                   (collect! (ecase ch
-                               (#\^ (list -1 0))
-                               (#\> (list 0 1))
-                               (#\v (list 1 0))
-                               (#\< (list 0 -1)))))))))
-      (list (parse-map map) (parse-movements movements)))))
-#+#:excluded (parse-input)
+  (destructuring-bind (warehouse moves) (split-sequence:split-sequence "" strings :test 'string=)
+    (list (prog1-let x (make-warehouse :grid (make-hash-table :test 'equal))
+            (doeseq (i s warehouse)
+              (doeseq (j ch s)
+                (setf (gethash (list i j) ~x.grid) ch)
+                (if (char= ch #\@) (setf ~x.robot (list i j))))))
+          (looping
+            (dolist (s moves)
+              (doseq (ch s)
+                (collect! (ecase ch
+                            (#\^ (list -1 0))
+                            (#\> (list 0 1))
+                            (#\v (list 1 0))
+                            (#\< (list 0 -1))))))))))
 
-(defun move-all (&optional (input (parse-input)))
-  (destructuring-bind ((map (ri rj)) movements) input
-    (doseq ((di dj) movements)
-      (labels ((move (i j &aux (pos (list i j)))
-                 (let1 pos1 (list (+ i di) (+ j dj))
-                   #+#:excluded (dbgl pos1 (gethash pos1 map))
-                   (cond ((char= (gethash pos1 map) #\.) (rotatef (gethash pos1 map)
-                                                                  (gethash pos map))
-                                                         t)
-                         ((char= (gethash pos1 map) #\O) (when (move (+ i di) (+ j dj))
-                                                           (rotatef (gethash pos1 map)
-                                                                    (gethash pos map))
-                                                           t))
-                         (t (assert (char= (gethash pos1 map) #\#)))))))
-        #+#:excluded (dbgl ri rj di dj)
-        (if (move ri rj)
-            (setf ri (+ ri di) rj (+ rj dj)))))
-    (result map)))
-(move-all (parse-input))
 
-(defun result (map)
+(defun moving-vertically? (dir) (/= ~dir.car 0))
+(defun moving-left? (dir) (< ~dir.cadr 0))
+(defun moving-right? (dir) (> ~dir.cadr 0))
+
+(defun move-straight (pos dir)
+  (list (+ ~pos.car ~dir.car) (+ ~pos.cadr ~dir.cadr)))
+(defun right-of (pos) (list ~pos.car (1+ ~pos.cadr)))
+(defun left-of (pos) (list ~pos.car (1- ~pos.cadr)))
+
+
+(defun can-move? (warehouse dir)
+  (recursively ((pos ~warehouse.robot))
+    (let1 next (move-straight pos dir)
+      (ecase (gethash next ~warehouse.grid)
+        (#\. t)
+        (#\# nil)
+        (#\O (recur next))
+        (#\[ (if (moving-vertically? dir)
+                 (and (recur next)
+                      (recur (right-of next)))
+                 (recur next)))
+        (#\] (if (moving-vertically? dir)
+                 (and (recur next)
+                      (recur (left-of next)))
+                 (recur next)))))))
+
+(defun move-robot! (warehouse dir)
+  (assert (can-move? warehouse dir))
+  (flet ((swap (pos1 pos2)
+           (rotatef (gethash pos1 ~warehouse.grid)
+                    (gethash pos2 ~warehouse.grid))))
+    (recursively ((pos ~warehouse.robot))
+      (let1 next (move-straight pos dir)
+        (ecase (gethash next ~warehouse.grid)
+          (#\. (swap pos next))
+          (#\O (recur next)                ; push small box out
+               (swap pos next))            ; move into the empty space
+          (#\[ (cond
+                 ((moving-vertically? dir)
+                   (recur next)            ; push first half-box out
+                   (recur (right-of next)) ; push second half-box out
+                   (swap pos next)         ; move into the empty space
+                   )
+                 (t
+                   (recur next)            ; make box out
+                   (swap pos next))))      ; move into the empty space
+          (#\] (cond
+                 ((moving-vertically? dir)
+                   (recur next)            ; push first half-box out
+                   (recur (left-of next))  ; push second half-box out
+                   (swap pos next)         ; move into the empty space
+                   )
+                 (t
+                   (recur next)            ; make room
+                   (swap pos next)         ; move into the empty space
+                   ))))
+        next))))
+
+(defun push-boxes (&optional (input (parse-input)))
+  (destructuring-bind (w moves) input
+    (doseq (dir moves)
+      (when (can-move? w dir)
+        (setf ~w.robot (move-robot! w dir))))
+    w))
+
+
+(defun result (warehouse)
   (looping
-    (dohash ((i j) ch map)
-      (when (char= ch #\O)
+    (dohash ((i j) ch ~warehouse.grid)
+      (when (find ch "O[")
         (sum! (+ (* i 100) j))))))
 
-(defun parse-input (&optional (strings (uiop:read-file-lines #P"src/2024/day15.txt")))
-  (destructuring-bind (map movements) (split-sequence:split-sequence "" strings :test 'string=)
-    (flet ((parse-map (strings)
-             (let  ((map (make-hash-table :test 'equal))
-                    (robot nil))
-               (doseq ((i s) (enumerate strings))
-                 (doseq ((j ch) (enumerate s))
-                   (ecase ch
-                     (#\# (setf (gethash (list i (* j 2)) map) ch
-                                (gethash (list i (1+ (* j 2))) map) ch))
-                     (#\. (setf (gethash (list i (* j 2)) map) ch
-                                (gethash (list i (1+ (* j 2))) map) ch))
-                     (#\O (setf (gethash (list i (* j 2)) map) #\[
-                                (gethash (list i (1+ (* j 2))) map) #\]))
-                     (#\@ (setf (gethash (list i (* j 2)) map) #\@
-                                (gethash (list i (1+ (* j 2))) map) #\.)))
-                   (if (char= ch #\@) (setf robot (list i (* j 2))))))
-               (list map robot)))
-           (parse-movements (strings)
-             (looping
-               (dolist (s strings)
-                 (doseq (ch s)
-                   (collect! (ecase ch
-                               (#\^ (list -1 0))
-                               (#\> (list 0 1))
-                               (#\v (list 1 0))
-                               (#\< (list 0 -1)))))))))
-      (list (parse-map map) (parse-movements movements)))))
-#+#:excluded (parse-input)
 
-(defun move-all (&optional (input (parse-input)))
-  (destructuring-bind ((map (ri rj)) movements) input
-    (doseq ((di dj) movements)
-      (labels ((moving-vertically? () (/= di 0))
-               (moving-left? () (< dj 0))
-               (moving-right? () (> dj 0))
-               (straight-from (pos) (list (+ (car pos) di) (+ (cadr pos) dj)))
-               (right-of (pos) (list (car pos) (1+ (cadr pos))))
-               (left-of (pos) (list (car pos) (1- (cadr pos))))
-               (swap (pos1 pos2) (rotatef (gethash pos1 map) (gethash pos2 map)) t)
-               (can-move? (pos)
-                 (cond ((char= (gethash pos map) #\.) t)
-                       ((char= (gethash pos map) #\#) nil)
-                       ((char= (gethash pos map) #\@) (can-move? (straight-from pos)))
-                       ((char= (gethash pos map) #\[)
-                        (cond ((moving-vertically?) (and (can-move? (straight-from pos))
-                                                         (can-move? (right-of (straight-from pos)))))
-                              (t (can-move? (straight-from pos)))))
-                       ((char= (gethash pos map) #\])
-                        (cond ((moving-vertically?) (and (can-move? (straight-from pos))
-                                                         (can-move? (left-of (straight-from pos)))))
-                              (t (can-move? (straight-from pos)))))
-                       (t (error "Cannot be here"))))
-               (move (pos)
-                 (cond ((char= (gethash pos map) #\.) #+#:excluded (error "NEVER ."))
-                       ((char= (gethash pos map) #\#) (error "NEVER #"))
-                       ((char= (gethash pos map) #\@) (move (straight-from pos))
-                                                      (swap pos (straight-from pos)))
-                       ((char= (gethash pos map) #\[)
-                        (cond ((moving-vertically?) (move (straight-from pos))
-                                                    (move (right-of (straight-from pos)))
-                                                    (swap pos (straight-from pos))
-                                                    (swap (right-of pos) (straight-from (right-of pos))))
-                              (t (move (straight-from pos))
-                                 (swap pos (straight-from pos)))))
-                       ((char= (gethash pos map) #\])
-                        (cond ((moving-vertically?) (move (straight-from pos))
-                                                    (move (left-of (straight-from pos)))
-                                                    (swap pos (straight-from pos))
-                                                    (swap (left-of pos) (straight-from (left-of pos))))
-                              (t (move (straight-from pos))
-                                 (swap pos (straight-from pos)))))
-                       (t (error "Cannot be here")))))
-        ; (dbgl ri rj di dj)
-        (assert (char= (gethash (list ri rj) map) #\@))
-        ; (display map)
-        ; (break)
-        (when (can-move? (list ri rj))
-          (move (list ri rj))
-          (setf ri (+ ri di) rj (+ rj dj)))))
-    #+#:excluded (display map)
-    (dbg (result map))
-    #+#:excluded (display map)))
-(move-all (parse-input))
+(defun massage-input (&optional (strings (uiop:read-file-lines #P"src/2024/day15.txt")))
+  (mapcar (fn (s)
+            (~> s
+              (cl-ppcre:regex-replace-all "#" ~ "##")
+              (cl-ppcre:regex-replace-all "\\." ~ "..")
+              (cl-ppcre:regex-replace-all "O" ~ "[]")
+              (cl-ppcre:regex-replace-all "@" ~ "@.")))
+          strings))
+#+#:excluded (massage-input)
 
-(defun display (map &aux (h 10) (w (* h 2)))
-  (dotimes (i h)
-    (dotimes (j w)
-      (pr (gethash (list i j) map)))
-    (pr #\Newline)))
 
-(defun result (map &aux (h 50) (w (* h 2)))
-  (looping
-    (dotimes (i h)
-      (dotimes (j w)
-        (when (char= (gethash (list i j) map) #\[)
-          (let1 di (min i #+#:excluded (- h i 1))
-            (let1 dj (min j #+#:excluded (- w j 2))
-              #+#:excluded (dbgl i j h w di dj)
-              ; (break)
-              (sum! (+ (* 100 di) dj)))))))))
+(define-solution (2024 15) (strings)
+  (values (~> strings parse-input push-boxes result)
+          (~> strings massage-input parse-input push-boxes result)))
+#+#:excluded (time (solution-run))
 
-; 19125000 too high
-; 18872500 too high
-
-(define-solution (2024 15) (input parse-input)
-  (values (looping
-            (doseq ((a b prize) input)
-              (awhen (solve a b prize)
-                (sum! it))))
-          (looping
-            (doseq ((a b (px py)) input)
-              (awhen (solve a b (list (+ px 10000000000000) (+ py 10000000000000)))
-                (sum! it))))))
-
-(define-test (2024 15) (39290 73458657399094))
+(define-test (2024 15) (1406392 1429013))
