347a09a464544bc9a3137f1edbdaad3417239ed5
Matteo Landi <matteo@matteolandi.net>
2025-12-02 23:06:59 +0100

    2025/02

---
+++ src/2025/day02.lisp
@@ -0,0 +1,39 @@
+(defpackage :aoc/2025/02 #.cl-user::*aoc-use*)
+(in-package :aoc/2025/02)
+
+(defun read-ranges (&optional (strings (uiop:read-file-lines #P"src/2025/day02.txt")))
+  (looping
+    (dolist (s (split-sequence:split-sequence #\, (first strings)))
+      (destructuring-bind (a b) (split-sequence:split-sequence #\- s)
+        (collect! (list (parse-integer a)
+                        (parse-integer b)))))))
+
+(defun sum-invalid-ids (&optional (ranges (read-ranges)) (invalidp 'invalid?))
+  (looping
+    (doseq ((a b) ranges)
+      (dorangei (n a b)
+        (when (funcall invalidp n)
+          (sum! n))))))
+
+(defun invalid? (n)
+  (let1 s (spr n)
+    (and (evenp (length s))
+         (destructuring-bind (a b) (split s (/ (length s) 2))
+           (string= a b)))))
+
+(defun invalid-p2? (n &aux (s (spr n)))
+  (looping
+    (dorange (d 1 (length s))
+      (when (dividesp d (length s))
+        (destructuring-bind (first . rest) (subdivide s d)
+          (thereis!
+            (looping
+              (dolist (other rest)
+                (always! (equal first other))))))))))
+
+
+(define-solution (2025 02) (ranges read-ranges)
+  (values (sum-invalid-ids ranges 'invalid?)
+          (sum-invalid-ids ranges 'invalid-p2?)))
+
+(define-test (2025 02) (5398419778 15704845910))
